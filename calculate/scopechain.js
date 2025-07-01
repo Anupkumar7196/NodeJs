714e260df3adafc3fd739330dfdc00fdcef7e8e7
Anup kumar <anupk7196@gmail.com>
2025-07-01 16:58:04 +0530

    update

---
+++ calculate/scopechain.js
@@ -0,0 +1,33 @@
+// function a(){
+
+//     console.log("This is the function a ");
+//     console.log(b);
+  
+//     function c(){
+//         console.log("This is the function c ");
+        
+//         console.log(b);
+        
+//     }
+//     c();
+    
+// }
+// let b = 10;
+
+// a();
+
+// require("./path")
+// All the code of the module is wrapped inside a function(IIFE)
+
+
+// (function (module) {
+//     // All the code of the module runs inside here
+//    function calculateMultiply1(a, b){
+//     const result = a*b;
+//     console.log(result);
+    
+//    }
+//    module.exports = {calculateMultiply1};
+//    calculateMultiply1();
+
+// }) (module.exports);
---
+++ synchronous.js
@@ -0,0 +1,8 @@
+var a= 1078698;
+var b= 20986;
+function multiply(x, y){
+    const result = a*b;
+    return result;
+}
+var c = multiply(a,b);
+console.log("The result ", c);
